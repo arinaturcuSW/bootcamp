c7b380cca53a8aa4f2774688df6dda29dc40e798
Arina Turcu <arina.turcu@softwire.com>
2023-06-27 10:30:36 +0300

    fizzbuzz done

--- index.ts
+++ index.ts
@@ -1,31 +1,45 @@
-import {before} from "node:test";
+import {Interface} from "readline";
+import {isNumberObject} from "util/types";
+import {isNullOrUndefined} from "util";
 
-function fizzbuzz(): void {
-    const numbers: number[] = Array.from({length: 143}, (x, i) => i + 1);
+const readline = require("readline");
+
+const modMap: {[key: number]: string} = {
+    3: 'Fizz',
+    5: 'Buzz',
+    7: 'Bang',
+    11: 'Bong',
+    13: 'Fezz'
+};
+
+function fizzbuzz(maxBound: number, rules: number[]): void {
+    const numbers: number[] = Array.from({length: maxBound}, (x, i) => i + 1);
 
     numbers.forEach((num: number): void => {
         let output: string[] = [];
 
-        const modMap: {[key: number]: string} = {
-            3: 'Fizz',
-            5: 'Buzz',
-            7: 'Bang',
-            11: 'Bong',
-            13: 'Fezz'
-        }
-
         Object.keys(modMap).forEach((key: string) => {
+            if (!rules.includes(Number(key))) {
+                return;
+            }
+
             if (num % Number(key) === 0) {
                 if (Number(key) === 11) {
                     output = [];
                 }
 
                 if (Number(key) === 13) {
-                    const firstB = output.find(s => s.startsWith("B"));
+                    const firstB: string | undefined = output.find(s => s.startsWith("B"));
 
-                    const index = output.indexOf(firstB);
+                    const index: number = firstB ? output.indexOf(firstB) : -1;
+                    const outputCopy: string[] = [...output];
 
-                    // output. TODO
+                    if (index > -1) {
+                        const left: string[] = output.splice(0, index);
+                        const right: string[] = outputCopy.splice(index);
+
+                        output = left.concat([modMap[Number(key)]], right)
+                    }
 
                     return;
                 }
@@ -34,14 +48,34 @@ function fizzbuzz(): void {
             }
         })
 
-
-
         if (num % 17 === 0) {
             output.reverse();
         }
 
-        console.log(output.join(''));
+        if (output.length) {
+            console.log(output.join(''));
+            return;
+        }
+
+        console.log(num);
     });
 }
 
-fizzbuzz();
+const rl: Interface =
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+rl.question("Enter the max value and the desired rules: \n", function (input: string): void {
+    console.log("You entered " + input);
+
+    const inputArray: number[] = input.split(' ').map((i: string) => Number(i));
+
+    const maxBound = inputArray[0];
+    const rules = inputArray.splice(1);
+
+    fizzbuzz(maxBound, rules);
+
+    rl.close();
+});
