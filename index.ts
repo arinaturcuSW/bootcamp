bbb4e3ffdfcf8a817e78ee43e0fe93620610d773
Arina Turcu <arina.turcu@softwire.com>
2023-06-27 11:48:26 +0300

    code cleanup

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 import {Interface} from "readline";
-import {isNumberObject} from "util/types";
-import {isNullOrUndefined} from "util";
-
-const readline = require("readline");
+import * as readline from "readline";
 
 const modMap: {[key: number]: string} = {
     3: 'Fizz',
@@ -12,41 +9,46 @@ const modMap: {[key: number]: string} = {
     13: 'Fezz'
 };
 
+function range(upperBound: number): number[] {
+    return Array.from({length: upperBound}, (x, i) => i + 1);
+}
+
+function insertElementAtIndex(array: string[], index: number, element: string): string[] {
+    if (index <= -1) {
+        return array;
+    }
+
+    const left: string[] = [...array].splice(0, index);
+    const right: string[] = [...array].splice(index);
+
+    return left.concat([element], right);
+}
+
 function fizzbuzz(maxBound: number, rules: number[]): void {
-    const numbers: number[] = Array.from({length: maxBound}, (x, i) => i + 1);
+    const numbers: number[] = range(maxBound);
 
     numbers.forEach((num: number): void => {
         let output: string[] = [];
 
         Object.keys(modMap).forEach((key: string) => {
-            if (!rules.includes(Number(key))) {
+            if (!rules.includes(Number(key)) || num % Number(key) !== 0) {
                 return;
             }
 
-            if (num % Number(key) === 0) {
-                if (Number(key) === 11) {
-                    output = [];
-                }
-
-                if (Number(key) === 13) {
-                    const firstB: string | undefined = output.find(s => s.startsWith("B"));
-
-                    const index: number = firstB ? output.indexOf(firstB) : -1;
-                    const outputCopy: string[] = [...output];
-
-                    if (index > -1) {
-                        const left: string[] = output.splice(0, index);
-                        const right: string[] = outputCopy.splice(index);
-
-                        output = left.concat([modMap[Number(key)]], right)
-                    }
-
-                    return;
-                }
-
-                output.push(modMap[Number(key)]);
+            if (Number(key) === 11) {
+                output = [];
             }
-        })
+
+            if (Number(key) === 13) {
+                const firstBWord: string | undefined = output.find(s => s.startsWith("B"));
+                const index: number = firstBWord ? output.indexOf(firstBWord) : -1;
+
+                output = insertElementAtIndex(output, index, modMap[Number(key)]);
+                return;
+            }
+
+            output.push(modMap[Number(key)]);
+        });
 
         if (num % 17 === 0) {
             output.reverse();
@@ -61,13 +63,12 @@ function fizzbuzz(maxBound: number, rules: number[]): void {
     });
 }
 
-const rl: Interface =
-    readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+const rl: Interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-rl.question("Enter the max value and the desired rules: \n", function (input: string): void {
+rl.question("Enter the max value followed by the desired rules: \n", function (input: string): void {
     console.log("You entered " + input);
 
     const inputArray: number[] = input.split(' ').map((i: string) => Number(i));
